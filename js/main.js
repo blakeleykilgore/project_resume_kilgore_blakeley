325070919bce92db485c33effe80916c776928f7
Blakeley Kilgore <blakeleykilgore@gmail.com>
2016-11-22 13:47:45 -0600

    Add jQuery color-strip plugin
    This commit will also remove the newest version of jQuery and replace it
    with an older version to avoid conflicts with the color-strip.

--- js/main.js
+++ js/main.js
@@ -19,11 +19,29 @@ $(document).ready(function () {
             scrollTop: target
         }, 1100);
     });
+// Add tooltip to footer links
     $('.tooltip').tooltipster({
         animation: 'grow',
         animationDuration: 550,
         delay: 250,
         theme: 'tooltipster-shadow',
-        plugins: ['follower'],
+        plugins: ['follower']
+    });
+// Add color strip
+    $('#colorstrip1').colorstrip({
+        minInterval: 6000,
+        maxInterval: 12000,
+        minWidth: 10,
+        maxWidth: 80,
+        opacity: 0.5,
+        colors: ['#dedfe1', '#466c9d', '#223f5f', '#f97849', '#511799', '#6792bf', '#dfbdb7']
+    });
+    $('#colorstrip2').colorstrip({
+        minInterval: 6000,
+        maxInterval: 12000,
+        minWidth: 10,
+        maxWidth: 80,
+        opacity: 0.5,
+        colors: ['#dedfe1', '#466c9d', '#223f5f', '#f97849', '#511799', '#6792bf', '#dfbdb7']
     });
 });
