bac4af61b78a4ad1d7a988721e283dd1d0366b37
Blakeley Kilgore <blakeleykilgore@gmail.com>
2016-11-22 11:47:46 -0600

    Add jQuery to change nav links on hover

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
+// Nav Hover
+    $('nav li').hover(function () {
+        $(this).find('a').css('color', '#6c9bc7');
 
+    }, function () {
+        $(this).find('a').css('color', '');
+    });
 });
